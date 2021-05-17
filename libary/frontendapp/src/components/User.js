3ce7cf5d7aeff746e01fcc81726529e878676075
geekbrains <ayankeav@gmail.com>
2021-05-17 21:16:05 +0300

    add menu and footer lesn 2 done

--- libary/frontendapp/src/components/User.js
+++ libary/frontendapp/src/components/User.js
@@ -18,18 +18,39 @@ const UserItem = ({user}) => {
 
 const UserList = ({users}) => {
    return (
-       <table>
-           <th>
-               First name
-           </th>
-           <th>
-               Last Name
-           </th>
-           <th>
-               Email
-           </th>
-           {users.map((user) => <UserItem user={user} />)}
-       </table>
+       <div className="outer">
+           <div className="header">
+               <ul>
+                   <li><a href="#">Главная</a></li>
+                   <li><a href="#">О нас</a></li>
+                   <li><a href="#">Галерея</a></li>
+                   <li><a href="#">Контакты</a></li>
+               </ul>
+           </div>
+           <div className="inner">
+               <table className="Table">
+                   <th>
+                       User name
+                   </th>
+                   <th>
+                       First name
+                   </th>
+                   <th>
+                       Last Name
+                   </th>
+                   <th>
+                       Email
+                   </th>
+                   {users.map((user) => <UserItem user={user}/>)}
+
+               </table>
+           </div>
+           <div className="footer">
+
+               <p>this footer</p>
+           </div>
+
+       </div>
    )
 }
 
